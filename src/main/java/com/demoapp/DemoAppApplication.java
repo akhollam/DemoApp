850a8ce5bd1f08752ca2f34f9598fe4f316e6b9c
Abhijeet Khollam <akhollam@psionline.com>
2022-05-08 12:05:54 +0530

    Made a controller;

--- src/main/java/com/demoapp/DemoAppApplication.java
+++ src/main/java/com/demoapp/DemoAppApplication.java
@@ -3,6 +3,7 @@ package com.demoapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Controller
 @SpringBootApplication
 public class DemoAppApplication {
 
